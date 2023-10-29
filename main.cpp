700df64b6f3d122e6c2c28936d5969c541dfa3ee
Brandon <brandon@pve-ubuntu.tailcf134.ts.net>
2023-10-29 11:41:54 -0700

    Basic Vendor Parsing

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "nlohmann/json.hpp" //for json parsing
+#include <fstream>
+#include <iostream>
+
+using json = nlohmann::json;
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    //string fileName = argv[1];
+
+    ifstream file("sample.json");
+    json pcap = json::parse(file);
+
+    for (int i = 0; i < pcap.size(); i++) {
+        cout << "Destination: " << pcap[i]["_source"]["layers"]["eth"]["eth.dst_tree"]["eth.dst.oui_resolved"] << endl;
+        cout << "Source: " << pcap[i]["_source"]["layers"]["eth"]["eth.src_tree"]["eth.src.oui_resolved"] << endl;
+        // json packet = json::parse(pcap[i]);
+    //     cout << packet["_index"] << endl;
+    }
+    //cout << pcap["_index"] << endl;
+
+}
