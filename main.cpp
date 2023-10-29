46909fe1494fff925c68fd299ae03ccf3a677bc6
Brandon <brandon@pve-ubuntu.tailcf134.ts.net>
2023-10-29 11:50:24 -0700

    Re-added CL args functionality

--- main.cpp
+++ main.cpp
@@ -6,17 +6,13 @@ using json = nlohmann::json;
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    //string fileName = argv[1];
+    string fileName = argv[1];
 
-    ifstream file("sample.json");
+    ifstream file(fileName);
     json pcap = json::parse(file);
 
     for (int i = 0; i < pcap.size(); i++) {
         cout << "Destination: " << pcap[i]["_source"]["layers"]["eth"]["eth.dst_tree"]["eth.dst.oui_resolved"] << endl;
         cout << "Source: " << pcap[i]["_source"]["layers"]["eth"]["eth.src_tree"]["eth.src.oui_resolved"] << endl;
-        // json packet = json::parse(pcap[i]);
-    //     cout << packet["_index"] << endl;
     }
-    //cout << pcap["_index"] << endl;
-
 }
